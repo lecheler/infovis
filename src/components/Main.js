2b2b7b5b8ac43bfa232f11599dbd1c89161af876
lecheler <l.lecheler@gmail.com>
2016-12-15 15:40:20 -0600

    add heroku postinstall for npm run build

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,7 @@ const App = React.createClass({
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React (npm run build worked!)</h2>
         </div>
         <p className="App-intro">
           { this.state.page }
