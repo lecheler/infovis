5b1d65f26847bdee07ba5b0d53cdb706193fe5a8
Luke <luke@Lucass-MacBook-Pro.local>
2016-12-15 19:16:30 -0600

    Start custom readme and link to FB

--- app.js
+++ app.js
@@ -16,24 +16,22 @@ app.use(express.static(path.resolve(__dirname, 'build')));
 // API Testing
 const pgp = require('pg-promise')();
 const cn = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    ssl: true
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  ssl: true
 };
 const db = pgp(cn);
 
 app.get('/api/questions', (req, res) => {
   db.any("select * from test_table", [true])
     .then(function (data) {
-        // success;
-        res.json(data);
+      res.json(data);
     })
     .catch(function (error) {
-        // error;
-        console.log(error);
+      console.log(error);
     });
 });
 
