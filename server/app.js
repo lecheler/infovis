8fa0789c1eca79851e80fe774b2bc8b68377551c
Luke <luke@Lucass-MacBook-Pro.local>
2016-12-15 20:16:37 -0600

    Favicon update and server js file restructure.

--- server/app.js
+++ server/app.js
@@ -10,18 +10,18 @@ const app = express();
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 // Serve static assets
-app.use(express.static(path.resolve(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 // API Routes
 require('./routes')(app);
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Luke's awesome app listening on port ${PORT}!`);
+  console.log(`Luke's awesome infovis-research app listening on port ${PORT}!`);
 });
