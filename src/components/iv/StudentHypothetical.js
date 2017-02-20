a5b9125de1ed7bd14ef8c2e0b194a89beba4936b
Lucas Lecheler <l.lecheler@gmail.com>
2017-02-20 15:09:11 -0600

    circle animate

--- src/components/iv/StudentHypothetical.js
+++ src/components/iv/StudentHypothetical.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Motion, spring} from 'react-motion';
 
+const springSetting1 = {stiffness: 180, damping: 20};
+const springSetting2 = {stiffness: 297, damping: 18};
+
 const scores = [
   Math.round(Math.random()*100),
   Math.round(Math.random()*100),
@@ -20,7 +23,6 @@ const StudentHypothetical = React.createClass({
   },
 
   handleMouseDown(e) {
-    console.log(e.nativeEvent)
     this.setState({yPosition: e.nativeEvent.layerY});
   },
 
@@ -50,41 +52,47 @@ const StudentHypothetical = React.createClass({
             );
           })
         }
-        <Motion style={{height: spring(450-this.state.yPosition), y: spring(this.state.yPosition)}}>
-          {({height, y}) =>
-            // children is a callback which should accept the current value of
-            // `style`
-            <div className="demo0"
-              onMouseDown={this.handleMouseDown}
-              onTouchStart={this.handleTouchStart}>
-              <div 
-                className="demo0-block" 
-                style={{
-                  height: height,
-                  WebkitTransform: `translate3d(0, ${y}px, 0)`,
-                  transform: `translate3d(0, ${y}px, 0)`,
-                }} 
-              />
+        <Motion 
+          style={{
+            height: spring(450-this.state.yPosition, springSetting1), 
+            y: spring(this.state.yPosition, springSetting1),
+            scale: spring(Math.random()+0.5, springSetting2)
+          }}>
+          {({height, y, scale}) =>
+            <div>
+              <div className="demo0"
+                onMouseDown={this.handleMouseDown}
+                onTouchStart={this.handleTouchStart}>
+                <div 
+                  className="demo0-block" 
+                  style={{
+                    height: height,
+                    WebkitTransform: `translate3d(0, ${y}px, 0)`,
+                    transform: `translate3d(0, ${y}px, 0)`,
+                  }} 
+                />
+              </div>
+              <div className='student-score-wrapper-lg'>
+                <div className='student-score-circle-avg-lg' 
+                  style={
+                    {
+                      borderColor: '#e6e6e6'
+                    }}>
+                  <div className='student-score-circle-lg' 
+                  style={
+                    {
+                      backgroundColor: '#AAD219',
+                      WebkitTransform: `scale(${scale})`,
+                      transform: `scale(${scale})`,
+                    }}
+                  >
+                  </div>
+                </div>
+              </div>
             </div>
           }
         </Motion>
-        <div className='student-score-wrapper-lg'>
-          <div className='student-score-circle-avg-lg' 
-            style={
-              {
-                borderColor: '#e6e6e6'
-              }}>
-            <div className='student-score-circle-lg' 
-            style={
-              {
-                backgroundColor: '#AAD219',
-                WebkitTransform: `scale(${0.8})`,
-                transform: `scale(${0.8})`,
-              }}
-            >
-            </div>
-          </div>
-        </div>
+        
       </div>
     );
   },
