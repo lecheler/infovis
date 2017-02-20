b6f4730079fe0b92745cbc85dc0578db1d0392b9
Lucas Lecheler <l.lecheler@gmail.com>
2017-02-20 14:59:47 -0600

    Add result circle to hypothetical

--- src/components/iv/StudentHypothetical.js
+++ src/components/iv/StudentHypothetical.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import {Motion, spring} from 'react-motion';
 
+const scores = [
+  Math.round(Math.random()*100),
+  Math.round(Math.random()*100),
+  Math.round(Math.random()*100),
+  Math.round(Math.random()*100),
+  Math.round(Math.random()*100),
+  Math.round(Math.random()*100),
+  Math.round(Math.random()*100),
+  Math.round(Math.random()*100),
+  Math.round(Math.random()*100),
+  Math.round(Math.random()*100) 
+];
+
 const StudentHypothetical = React.createClass({
   getInitialState() {
-    return {open: false, yPosition: 0};
+    return {open: false, yPosition: 450};
   },
 
   handleMouseDown(e) {
@@ -18,7 +31,25 @@ const StudentHypothetical = React.createClass({
 
   render() {
     return (
-      <div>
+      <div className='bar-container'>
+        {
+          scores.map((scores, key) => {
+            const val = (scores/100) * 450;
+            
+            return (
+              <div key={key} className="demo0">
+                <div 
+                  className="demo0-block" 
+                  style={{
+                    height: 450-val,
+                    WebkitTransform: `translate3d(0, ${val}px, 0)`,
+                    transform: `translate3d(0, ${val}px, 0)`,
+                  }} 
+                />
+              </div>
+            );
+          })
+        }
         <Motion style={{height: spring(450-this.state.yPosition), y: spring(this.state.yPosition)}}>
           {({height, y}) =>
             // children is a callback which should accept the current value of
@@ -37,6 +68,23 @@ const StudentHypothetical = React.createClass({
             </div>
           }
         </Motion>
+        <div className='student-score-wrapper-lg'>
+          <div className='student-score-circle-avg-lg' 
+            style={
+              {
+                borderColor: '#e6e6e6'
+              }}>
+            <div className='student-score-circle-lg' 
+            style={
+              {
+                backgroundColor: '#AAD219',
+                WebkitTransform: `scale(${0.8})`,
+                transform: `scale(${0.8})`,
+              }}
+            >
+            </div>
+          </div>
+        </div>
       </div>
     );
   },
