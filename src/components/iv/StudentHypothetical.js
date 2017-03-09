c5d23528edad700862e8a2d815afb2c7f85a7400
Lucas Lecheler <l.lecheler@gmail.com>
2017-03-08 21:15:36 -0600

    Color change based on inner circle scale

--- src/components/iv/StudentHypothetical.js
+++ src/components/iv/StudentHypothetical.js
@@ -6,22 +6,38 @@ import data from '../data';
 
 const springSetting1 = {stiffness: 180, damping: 20};
 const springSetting2 = {stiffness: 297, damping: 18};
+const studentIndex = 3;
 
 const StudentHypothetical = React.createClass({
   //    const s = this.state.regression[this.state.regression.length-1]/115;
   
 
   componentWillMount() {
-    const scores = data.STUDENT_CHARTS.datasets[4].data;
+    const scores = data.STUDENT_CHARTS.datasets[studentIndex].data;
     const aims = this.getAimLine(scores);
     const regression = this.getRegressionLine(scores);
     const scale = regression[regression.length-1]/115;
+    const color = this.getRGBForScale(scale);
 
     this.setState({ 
       scores: scores,
       aims: aims,
       scale: scale,
+      red: color.red,
+      green: color.green,
+      blue: color.blue
     });
+  },
+
+  getRGBForScale(scale) {
+    console.log(scale)
+    let color = {red: 200, green: 0, blue: 84};
+    if (scale > 1.10) {
+      color = {red: 170, green: 210, blue: 25};
+    } else if (scale > 0.90) {
+      color = {red: 32, green: 168, blue: 204};
+    }
+    return color;
   },
 
   getAimLine(points) {
@@ -41,7 +57,7 @@ const StudentHypothetical = React.createClass({
     return {
       open: false, 
       yPosition: 450,
-      scores: []
+      scores: [],
     };
   },
 
@@ -66,17 +82,19 @@ const StudentHypothetical = React.createClass({
     const nextScore = (450-e.nativeEvent.layerY)/450 * 150;
     const arr = []; //data.STUDENT_CHARTS.datasets[1].data;
     for (var index = 0; index < data.STUDENT_CHARTS.datasets[1].data.length; index++) {
-      arr.push(data.STUDENT_CHARTS.datasets[4].data[index]);
+      arr.push(data.STUDENT_CHARTS.datasets[studentIndex].data[index]);
     }
     arr.push(nextScore);
 
     const newRegression = this.getRegressionLine(arr);
-
-  //  console.log(newRegression);
-
+    const scale = newRegression[newRegression.length-1]/115;
+    const color = this.getRGBForScale(scale);
     this.setState({
       yPosition: e.nativeEvent.layerY,
-      scale: newRegression[newRegression.length-1]/115
+      scale: scale,
+      red: color.red,
+      green: color.green,
+      blue: color.blue
     });
   },
 
@@ -118,9 +136,11 @@ const StudentHypothetical = React.createClass({
             height: spring(450-this.state.yPosition, springSetting1), 
             y: spring(this.state.yPosition, springSetting1),
             scale: spring(this.state.scale, springSetting2),
-            color: 'red'
+            red: this.state.red,
+            green: this.state.green,
+            blue: this.state.blue
           }}>
-          {({height, y, scale, color}) =>
+          {({height, y, scale, red, green, blue}) =>
             <div>
               <div className="demo0"
                 onMouseDown={this.handleMouseDown}
@@ -143,7 +163,7 @@ const StudentHypothetical = React.createClass({
                   <div className='student-score-circle-lg' 
                   style={
                     {
-                      backgroundColor: '#ff0000',
+                      backgroundColor: `rgb(${red}, ${green}, ${blue})`,
                       WebkitTransform: `scale(${scale})`,
                       transform: `scale(${scale})`,
                     }}
