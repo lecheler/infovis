529bca7b9ff417dc4acea6ce37b6ded70ff8881e
Lucas Lecheler <l.lecheler@gmail.com>
2018-02-06 15:11:39 -0600

    Use Bootstrap 3.2 (using "latest" breaks functionality)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router'; 
+import { Router, Route, browserHistory } from 'react-router';
 import Main from './components/Main';
 import Question from './components/Question';
 import About from './components/About';
 import Survey from './components/Survey';
 import Feedback from './components/Feedback';
 import Directions from './components/Directions';
+
+import Portfolio from './components/Portfolio';
+
 
 import './App.css';
 
@@ -15,6 +18,7 @@ const App = React.createClass({
       <Router history={browserHistory} >
         <Route path="/" component={Main} />
         <Route path="/about" component={About} />
+        <Route path="/portfolio" component={Portfolio} />
         <Route path="/directions/:userID" component={Directions} />
         <Route path="/survey" component={Survey} />
         <Route path="/feedback/:userID" component={Feedback} />
